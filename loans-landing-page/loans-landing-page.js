fb0b53e524e0da20489a8c56dbf1f0473a0a15e6
Mamata <mamata@ajency.in>
2023-02-14 20:35:58 +0530

    Loans Landing Page - YouTube Player Implemented

--- loans-landing-page/loans-landing-page.js
+++ loans-landing-page/loans-landing-page.js
@@ -1,10 +1,11 @@
+// Banner Slider
 $(".llp-banner-slider").slick({
   arrows: false,
   dots:true,
   appendDots: '.llp-slick-dots'
 });
 
-
+// Lending Partners Carousel
 $(".llp-partners-carousel").slick({
   slidesToShow: 6,
   arrows:false,
@@ -28,3 +29,50 @@ $(".llp-partners-carousel").slick({
     },
   ],
 });
+
+//youtube video player
+function onPlayerReady(event){
+  //console.log("Player Ready!")
+  //console.log(event)
+}
+function stopVideo() {
+  player.stopVideo();
+}
+function playVideo() {
+  player.playVideo();
+}
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('llp_yt_player1', {
+    height: '390',
+    width: '640',
+    videoId: 'QyfulyhtkdA',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+if($('.llp-yt-player').is(":visible")) {
+  setTimeout(
+    function(){
+      // Youtube video player
+      var tag = document.createElement('script');
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }, 1000
+  );
+};
+
+$('.video-play-button').click( function() {
+  let videoPlayerParent = $(this).parents('.llp-video-player')
+  $(videoPlayerParent).find('.content').fadeOut()
+  $(videoPlayerParent).find('.video-thumbnail').fadeOut()
+  $(videoPlayerParent).addClass('player-enabled')
+  $(videoPlayerParent).find('#llp-yt-player').fadeIn()
+  playVideo()
+})
